47a89af53155716a4595ee2cbbc133c6b5c8ee12
Michael Ruggiero <mruggiero@printscan.com>
2022-06-23 22:25:06 -0400

    Changed project names, added a services project with a generate password service

---
+++ Services/Concrete/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+namespace StorePass.DataServer.Services;
+
+public static class PasswordGenerator
+{
+    const string alphanumericCharacters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        "abcdefghijklmnopqrstuvwxyz" +
+        "0123456789";
+
+    const string specialCharacters = @"`~!@#$%^&*()-_=+[]{}\|;:'"",.<>/?";
+
+    public static string Generate(int length)
+    {
+        var indexPositions = new int[length];
+
+        indexPositions = indexPositions
+            .Select(x => x = RandomNumberGenerator.GetInt32(0, alphanumericCharacters.Length))
+            .ToArray();
+
+        var passwordChars = indexPositions
+            .Select(x => alphanumericCharacters[x])
+            .ToArray();
+
+        return new string(passwordChars);
+    }
+}
